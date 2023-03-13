71f5c702bcb1cd50eac394039bc61b7e8986f40e
alex-neveroff <114777678+alex-neveroff@users.noreply.github.com>
2023-03-13 20:03:55 +0000

    Deploying to gh-pages from @ alex-neveroff/goit-js-hw-09@37fab320bd2310f5dbec6311d79a19619b17915f 🚀

---
+++ 01-color-switcher.ca9746cf.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),a=document.querySelector("body");t.disabled=!0;var d=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.ca9746cf.js.map
