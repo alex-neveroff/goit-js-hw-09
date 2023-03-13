37fab320bd2310f5dbec6311d79a19619b17915f
Alexandr Neveroff <neveroff1976@gmail.com>
2023-03-13 22:03:47 +0200

    Task 01 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+stopBtn.disabled = true;
+let timerId = null;
 
+startBtn.addEventListener('click', ClickedStartButton);
+stopBtn.addEventListener('click', ClickedStopButton);
+
+function ClickedStartButton() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  getNewColor();
+}
+
+function ClickedStopButton() {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(timerId);
+}
+
+function getNewColor() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
