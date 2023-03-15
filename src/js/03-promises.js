94e5b9b4448e4453d794b45fb2faa31abeea9e47
Alexandr Neveroff <neveroff1976@gmail.com>
2023-03-15 12:07:22 +0200

    Task 03 ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formDelay = document.querySelector('input[name="delay"]');
+const formStep = document.querySelector('input[name="step"]');
+const formAmount = document.querySelector('input[name="amount"]');
+const formButton = document.querySelector('button[type="submit"]');
+
+formButton.addEventListener('click', clickedFormButton);
+
+function clickedFormButton(event) {
+  event.preventDefault();
+  let currentDelay = Number(formDelay.value);
+  for (let i = 1; i <= formAmount.value; i += 1) {
+    createPromise(i, currentDelay).then(onResolve).catch(onReject);
+    currentDelay += Number(formStep.value);
   }
+  clearForm(currentDelay);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onResolve({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onReject({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+
+function clearForm(delay) {
+  setTimeout(() => {
+    formDelay.value = '';
+    formStep.value = '';
+    formAmount.value = '';
+  }, delay);
 }
